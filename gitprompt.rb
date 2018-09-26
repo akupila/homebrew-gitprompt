437363e6cf21d6dcaff76e76713f665bce01c4ff
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-26 08:38:51 +0200

    Brew formula update for gitprompt version v0.2.0

--- gitprompt.rb
+++ gitprompt.rb
@@ -1,9 +1,9 @@
 class Gitprompt < Formula
   desc "git status in the prompt"
   homepage "https://github.com/akupila/gitprompt"
-  url "https://github.com/akupila/gitprompt/releases/download/v0.1.1/gitprompt_0.1.1_darwin_amd64.tar.gz"
-  version "0.1.1"
-  sha256 "0fee93542a51af53e01bd04ee78938f8c2d97d77f54c06db94681a4a76183447"
+  url "https://github.com/akupila/gitprompt/releases/download/v0.2.0/gitprompt_0.2.0_darwin_amd64.tar.gz"
+  version "0.2.0"
+  sha256 "8133bf3619231f2819546d0ed18a5d2a778d26c5163af58cf3798bb73332b1ab"
 
   def install
     bin.install "gitprompt"
